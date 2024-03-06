69788045c86ae3e354267f14263ad2d6a65e298a
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2024-03-06 14:10:10 +0100

    Use Rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -43,6 +44,9 @@ module.exports = {
         DEFAULT: colors.neutral[500],
         dark: colors.neutral[700],
       }
+    },
+    fontFamily: {
+      sans: ["Rubik", ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: []
