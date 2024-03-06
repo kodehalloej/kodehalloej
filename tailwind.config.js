c3f23f8e47fa3cea075fe49a03118003f11da936
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2024-03-06 12:41:53 +0100

    Curate and name color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./_drafts/**/*.html",
@@ -8,8 +10,39 @@ module.exports = {
     "./*.html",
   ],
   theme: {
-    theme: {
-      extend: {},
+    colors: {
+      transparent: colors.transparent,
+      current: colors.current,
+      black: {
+        ish: colors.neutral[800],
+        DEFAULT: colors.black,
+      },
+      white: {
+        DEFAULT: colors.white,
+        ish: colors.neutral[100],
+      },
+      red: {
+        light: colors.red[400],
+        DEFAULT: colors.red[800],
+      },
+      cyan: colors.teal[300],
+      purple: colors.fuchsia[700],
+      green: {
+        light: colors.lime[300],
+        DEFAULT: colors.lime[600],
+      },
+      blue: {
+        light: colors.indigo[400],
+        DEFAULT: colors.indigo[800],
+      },
+      yellow: colors.yellow[200],
+      orange: colors.amber[600],
+      brown: colors.yellow[900],
+      grey: {
+        light: colors.neutral[400],
+        DEFAULT: colors.neutral[500],
+        dark: colors.neutral[700],
+      }
     },
   },
   plugins: []
