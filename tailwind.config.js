f3e6307428906e7c7c3b1cf10519f14572a1702d
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2024-03-25 13:16:01 +0100

    Add type scale and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,8 +45,31 @@ module.exports = {
         dark: colors.neutral[700]
       }
     },
+    spacing: {
+      0: "0",
+      0.25: "0.4375rem",
+      0.5: "0.875rem",
+      0.75: "1.3125rem",
+      1: "1.75rem",
+      1.25: "2.1875rem",
+      1.5: "2.625rem",
+      1.75: "3.0625rem",
+      2: "3.5rem",
+      9: "15.75rem"
+    },
     fontFamily: {
       sans: ["Rubik", ...defaultTheme.fontFamily.sans]
+    },
+    fontSize: {
+      xs: ["0.8125rem", "1.375rem"],
+      sm: ["0.9375rem", "1.5rem"],
+      base: ["1.125rem", "1.75rem"],
+      lg: ["1.375rem", "1.75rem"],
+      xl: ["1.625rem", "2rem"],
+      "2xl": ["1.9375rem", "2.375rem"],
+      "3xl": ["2.3125rem", "2.75rem"],
+      "4xl": ["2.8125rem", "3.125rem"],
+      "5xl": ["3.375rem", "3.75rem"]
     }
   },
   plugins: []
