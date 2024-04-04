7ed23c075bba208d7a9c85c729587d4d14b3701e
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2024-04-04 11:29:25 +0200

    Add mail and phone to contact information
    This includes encoding/decoding scripts which hopefully make it a bit
    harder for bots to harvest the information.

---
+++ assets/js/encode.js
@@ -0,0 +1,16 @@
+function make2DigitsLong(value){
+    return value.length === 1 
+        ? '0' + value
+        : value;
+}
+
+function encodeString(nonEncodedString, key) {
+    var encodedString = make2DigitsLong(key.toString(16));
+
+    for(var n=0; n < nonEncodedString.length; n++) {
+        encodedString += make2DigitsLong(
+            (nonEncodedString.charCodeAt(n) ^ key).toString(16));
+    }
+    
+    return encodedString;
+}
