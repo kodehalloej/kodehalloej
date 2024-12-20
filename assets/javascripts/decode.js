5f0def66fef75d19a678e5063006d95bec892967
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2024-12-20 10:53:53 +0100

    Adjust formatting in JavaScript files

--- assets/javascripts/decode.js
+++ assets/javascripts/decode.js
@@ -1,39 +1,47 @@
 function decodeString(encodedString) {
-    var result = ""; 
-    var key = parseInt(encodedString.substr(0, 2), 16);
-    
-    for (var n = 2; n < encodedString.length; n += 2) {
-        result += String.fromCharCode(parseInt(encodedString.substr(n, 2), 16) ^ key);
-    }
-    
-    return result;
+  var result = "";
+  var key = parseInt(encodedString.substr(0, 2), 16);
+
+  for (var n = 2; n < encodedString.length; n += 2) {
+    result += String.fromCharCode(
+      parseInt(encodedString.substr(n, 2), 16) ^ key,
+    );
+  }
+
+  return result;
 }
 
 function formatPhoneNumber(phoneNumber) {
-    return phoneNumber.substr(0,3) + " " +
-        phoneNumber.substr(3,2) + " " +
-        phoneNumber.substr(5,2) + " " +
-        phoneNumber.substr(7,2) + " " +
-        phoneNumber.substr(9,2);
+  return (
+    phoneNumber.substr(0, 3) +
+    " " +
+    phoneNumber.substr(3, 2) +
+    " " +
+    phoneNumber.substr(5, 2) +
+    " " +
+    phoneNumber.substr(7, 2) +
+    " " +
+    phoneNumber.substr(9, 2)
+  );
 }
 
 function updateLink(link) {
-    var s = link.dataset.encoded.split(":");
-    
-    s[1] = decodeString(s[1]);
-    link.href = s[0] + ":" + s[1];
-    link.textContent = s[0] === "tel" ? formatPhoneNumber(s[1]) : s[1];
-    delete link.dataset.encoded;
+  var s = link.dataset.encoded.split(":");
+
+  s[1] = decodeString(s[1]);
+  link.href = s[0] + ":" + s[1];
+  link.textContent = s[0] === "tel" ? formatPhoneNumber(s[1]) : s[1];
+  delete link.dataset.encoded;
 }
 
 function decodeLinks() {
-    var links = document.querySelectorAll("[data-encoded]");
+  var links = document.querySelectorAll("[data-encoded]");
 
-    for (var i = 0; i < links.length; i++) {
-        updateLink(links[i]);
-    }
+  for (var i = 0; i < links.length; i++) {
+    updateLink(links[i]);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    decodeLinks();
+  decodeLinks();
 });
--- assets/javascripts/encode.js
+++ assets/javascripts/encode.js
@@ -1,16 +1,15 @@
-function make2DigitsLong(value){
-    return value.length === 1 
-        ? '0' + value
-        : value;
+function make2DigitsLong(value) {
+  return value.length === 1 ? "0" + value : value;
 }
 
 function encodeString(nonEncodedString, key) {
-    var encodedString = make2DigitsLong(key.toString(16));
+  var encodedString = make2DigitsLong(key.toString(16));
 
-    for(var n=0; n < nonEncodedString.length; n++) {
-        encodedString += make2DigitsLong(
-            (nonEncodedString.charCodeAt(n) ^ key).toString(16));
-    }
-    
-    return encodedString;
+  for (var n = 0; n < nonEncodedString.length; n++) {
+    encodedString += make2DigitsLong(
+      (nonEncodedString.charCodeAt(n) ^ key).toString(16),
+    );
+  }
+
+  return encodedString;
 }
