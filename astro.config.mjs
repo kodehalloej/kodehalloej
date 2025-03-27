56802652b3c43db034ff54bd283faf2810dc3274
Daniel Møller Kristensen <85347107+kodehalloej@users.noreply.github.com>
2025-03-27 09:37:35 +0100

    Install Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: "prism",
   },
